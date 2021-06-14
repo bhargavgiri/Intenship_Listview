f7ab913265d88b960183d2232059c677fff9fe25
bhargavgiri <bhargavgiri86@gmail.com>
2021-06-14 11:54:54 +0530

    List

--- app/src/main/java/com/example/listview/Web.java
+++ app/src/main/java/com/example/listview/Web.java
@@ -20,7 +20,6 @@ public class Web extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_web);
 
